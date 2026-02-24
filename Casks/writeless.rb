06ee971e9fe241352a9a9917b918d974700d122e
Roman Kln <rk@romus.dev>
2026-02-24 15:24:24 +0400

    Ver 1.0.2

--- Casks/writeless.rb
+++ Casks/writeless.rb
@@ -1,6 +1,6 @@
 cask "writeless" do
-  version "1.0.1"
-  sha256 "e74ffdc88cb6f29907307e8a1765bbf11c3b48995efbe666cd04a024ae43488a"
+  version "1.0.2"
+  sha256 "f3da1d9eb9c05b2344504716f3483187bdda43fd641e0ca2f0ff426473b7a732"
 
   url "https://github.com/romus/writeless/releases/download/v#{version}/WriteLess-#{version}.zip"
   name "Write Less"
